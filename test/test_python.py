f2568042050cb900c7deec47736865e5a0bea275
AVNG <nguyen.vuan@wisseninfotech.com>
2024-09-13 17:36:49 +0700

    add method to call tests

--- test/test_python.py
+++ test/test_python.py
@@ -1,6 +1,9 @@
+from fastapi import testclient
+from app.main import app
 
-def inc(x:int):
-    return x + 1
+client = testclient.TestClient(app)
 
-def test_incriment():
-    assert inc(3) == 4
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"msg":"Hello World"}
