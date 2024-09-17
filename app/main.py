d5079f959971f68a4926b80630a620b3c3e247e5
AVNG <nguyen.vuan@wisseninfotech.com>
2024-09-17 17:34:55 +0700

    add place holder for CRUD operation

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+"""Api entry point"""
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,3 +6,22 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"msg":"Hello World"}
+
+@app.get("/api/tickets")
+def read_user_ticket():
+    """read all available tickets"""
+    raise NotImplementedError
+
+@app.post("/api/tickets")
+def create_user_ticket():
+    """create a new ticket"""
+    raise NotImplementedError
+
+@app.put("/api/tickets/{id}")
+def update_user_ticket():
+    """update a ticket information"""
+    raise NotImplementedError
+
+@app.delete("/api/tickets/{id}")
+def delete_user_ticket():
+    raise NotImplementedError
