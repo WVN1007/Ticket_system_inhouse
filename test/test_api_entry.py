fe88ca28cdd23c89a5f9076eb46f29346b1e52df
AVNG <nguyen.vuan@wisseninfotech.com>
2024-09-18 16:02:15 +0700

    add test for database connection

--- test/test_api_entry.py
+++ test/test_api_entry.py
@@ -1,5 +1,9 @@
 from fastapi import testclient
 from app.main import app
+from app.database import url_object
+from app.settings import app_config
+import psycopg
+from sqlalchemy import create_engine
 
 client = testclient.TestClient(app)
 
@@ -7,3 +11,26 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg":"Hello World"}
+
+def test_database_connection():
+    '''
+    Assert that the following conn raise no exception
+    '''
+    try:
+        DB_USERNAME = app_config["DB_USERNAME"]
+        DB_PASS = app_config["DB_PASS"]
+        DB_HOST = app_config["DB_HOST"]
+        DB_NAME = app_config["DB_NAME"]
+        DB_PORT = app_config["DB_PORT"]
+        print(f'{url_object}')
+        psycopg.connect(
+            host=DB_HOST,
+            dbname=DB_NAME,
+            user=DB_USERNAME,
+            password=DB_PASS,
+            port=DB_PORT,
+        )
+        create_engine(url_object,echo=True)
+    except Exception as e:
+        assert False, f"test_database_connection raise exception {e}"
+
