496eecce2d027c1923b22340e78113a534d1038b
AVNG <nguyen.vuan@wisseninfotech.com>
2024-09-23 12:27:03 +0700

    add routers for tickets, users, dev

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 """Api entry point"""
 
 from fastapi import FastAPI
-from .routers import users
+from .routers import users,tickets,devs
 
 app = FastAPI()
 
 app.include_router(users.router)
+app.include_router(tickets.router)
+app.include_router(devs.router)
 
 @app.get("/")
 def root():
