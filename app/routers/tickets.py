dfd0a38fde0b7f496355f97fedcff98efdce4ead
AVNG <nguyen.vuan@wisseninfotech.com>
2024-09-24 10:51:52 +0700

    add place holder for tickets creation

--- app/routers/tickets.py
+++ app/routers/tickets.py
@@ -2,6 +2,7 @@
 
 from enum import Enum
 from fastapi import APIRouter
+from ..schemas import TicketCreate
 
 class TicketType(str, Enum):
     ritm = "ritm"
@@ -23,3 +24,17 @@ async def read_tickets(type_name: TicketType):
         return [{"ritm_ticket1":"ritm_items_!"},{"ritm_ticket2":"ritem_data_@"}]
 
 # TODO: make CRUD for tickets
+# TODO: Tasks
+'''
+- [x] check how pydantic behave in GET method
+- [ ] create tests data in the database to test for GET method
+- [ ] write GET method to read from database
+'''
+
+@router.post("/{type_name}/")
+async def create_ticket(type_name:TicketType,ticket:TicketCreate):
+    if type_name is TicketType.inc:
+        return {"ticket":ticket, "type":"INC"}
+    if type_name is TicketType.ritm:
+        return {"ticket":ticket, "type":"RITM"}
+
