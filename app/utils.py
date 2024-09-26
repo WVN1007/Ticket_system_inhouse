3cd92096fb860e170b2e9968f1226d8ff9010443
AVNG <nguyen.vuan@wisseninfotech.com>
2024-09-26 19:39:26 +0700

    remove passlib due to deprecation

--- app/utils.py
+++ app/utils.py
@@ -1,10 +1,17 @@
-from passlib.context import CryptContext
+# from passlib.context import CryptContext
+import bcrypt
 
-context = CryptContext(schemes=["bcrypt"])
+# context = CryptContext(schemes=["bcrypt"])
 
 
-def hash_pwd(password: str):
-    return context.hash(password)
+# def hash_pwd(password: str):
+#     return context.hash(password)
 
 # TODO: switch from using passlib to bcrypt due to crypt will be deprecated
 # scr : https://www.geeksforgeeks.org/hashing-passwords-in-python-with-bcrypt/
+
+def hash_pwd(pwd: str):
+    byte = pwd.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hash = bcrypt.hashpw(byte,salt)
+    return hash
