db83e99766dc806fd276ecd4769abef4895d1fa3
AVNG <nguyen.vuan@wisseninfotech.com>
2024-09-28 16:05:49 +0700

    add check_pwd method and formatted with BLACK

--- app/utils.py
+++ app/utils.py
@@ -4,11 +4,19 @@ import bcrypt
 # switch from using passlib to bcrypt due to crypt will be deprecated
 # scr : https://www.geeksforgeeks.org/hashing-passwords-in-python-with-bcrypt/
 
+
 def hash_pwd(pwd: str | bytes):
-    if isinstance(pwd,str):
-        byte = pwd.encode('utf-8')
+    if isinstance(pwd, str):
+        byte = pwd.encode("utf-8")
     else:
         byte = pwd
     salt = bcrypt.gensalt()
-    hash = bcrypt.hashpw(byte,salt)
+    hash = bcrypt.hashpw(byte, salt)
     return hash
+
+
+# TODO: create password check method
+def check_pwd(input: str, hash: bytes) -> bool:
+    input_bytes = input.encode("utf-8")
+
+    return bcrypt.checkpw(input_bytes, hash)
