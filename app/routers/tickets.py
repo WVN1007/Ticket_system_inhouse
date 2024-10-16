9a68d6dac51f0216861c79d16c8810210ae4a8e1
AVNG <nguyen.vuan@wisseninfotech.com>
2024-10-16 10:37:00 +0700

    update endpoints to get all and get one ticket

--- app/routers/tickets.py
+++ app/routers/tickets.py
@@ -1,6 +1,7 @@
 """routers to tickets data"""
 
 from typing import List
+import uuid
 from fastapi import APIRouter, status, Depends
 from fastapi.exceptions import HTTPException
 from sqlalchemy.orm import Session
@@ -25,7 +26,7 @@ async def create_ticket(
 ):
     """this endpoint will create a tickets from an authenticated user"""
     new_ticket_data = ticket.model_dump()
-    print(new_ticket_data)
+    # print(new_ticket_data)
     try:
         user = db.execute(
             select(db_model.User).filter_by(uid=current_user.uid)
@@ -55,19 +56,27 @@ async def create_ticket(
     return db_ticket
 
 
-@router.get("/",response_model= List[schemas.TicketOut])
-async def get_tickets(  
+@router.get("/", response_model=List[schemas.TicketOut])
+async def get_tickets(
     db: Session = Depends(get_db),
     current_user=Depends(oauth_utils.get_current_staff)
     or Depends(oauth_utils.get_current_user),
 ):
-
     """this endpoints will gets ticket if the login user is authenticated"""
-    tickets = db.execute(select(db_model.Ticket)).all()
+    tickets = db.execute(select(db_model.Ticket)).scalars().all()
     return tickets
 
 
-@router.get("/{id}")
-async def get_ticket_w_id(id: str):
+@router.get("/{id}",response_model=schemas.TicketOut)
+async def get_ticket_w_id(
+    id: str,
+    db: Session = Depends(get_db),
+    current_user=Depends(oauth_utils.get_current_user)
+    or Depends(oauth_utils.get_current_staff),
+):
     """retrieve specific ticket with id"""
-    return {"msg": id}
+    uid = uuid.UUID(id)
+    ticket = db.execute(select(db_model.Ticket).filter_by(uid=uid)).scalar_one_or_none()
+    if ticket is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return ticket
