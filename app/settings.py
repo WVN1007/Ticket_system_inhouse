ae1c8cf644304e53ab132363b0c4abc0e2cfdd1d
WVN1007 <nguyen.vuan@wisseninfotech.com>
2024-11-18 10:31:04 +0700

    add Enumclass for handling env variables

--- app/settings.py
+++ app/settings.py
@@ -1,3 +1,19 @@
+from enum import Enum
 from dotenv import dotenv_values
 
 app_config = dotenv_values(dotenv_path=".env")
+
+class SupportDB(Enum):
+    ENGINE =  app_config['DB_ENGINE'] or "psycopg"
+    USERNAME = app_config['DB_USERNAME'] or "postgres"
+    HOST = app_config['DB_HOST'] or 'localhost'
+    PWD = app_config['DB_PASS'] or 'postgres'
+    PORT = app_config['DB_PORT'] or '5432'
+    DB_NAME = app_config['DB_NAME'] or 'postgres'
+
+class SupportSecurity(Enum):
+    SECRET_KEY = app_config['SECRET_KEY']
+    ALGORITHMS = app_config['ALGORITHMS']
+    ACCESS_TOKEN_EXPIRE_MINUTES = app_config['ACCESS_TOKEN_EXPIRE_MINUTES']
+
+
