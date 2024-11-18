a60f9a5332074c6785c1a0e7d77a5c711ac1dca4
WVN1007 <nguyen.vuan@wisseninfotech.com>
2024-11-18 11:02:26 +0700

    change appconfig to Support Enum classes

--- app/database.py
+++ app/database.py
@@ -2,15 +2,24 @@
 
 from sqlalchemy import URL, create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase, MappedAsDataclass
-from . import settings
-
+# from . import settings
+from .settings import SupportDB, SupportSecurity
 
 # create Base object to build tables
 class Base(MappedAsDataclass, DeclarativeBase):
     """subclassing will be converted to dataclasses"""
 
+app_config = {}
+app_config["DB_ENGINE"] = SupportDB.ENGINE.value
+app_config["DB_USERNAME"] = SupportDB.USERNAME.value
+app_config["DB_PASS"] = SupportDB.PWD.value
+app_config["DB_HOST"] = SupportDB.HOST.value
+app_config["DB_NAME"] = SupportDB.DB_NAME.value
+app_config["DB_PORT"] = SupportDB.PORT.value
+app_config["ACCESS_TOKEN_EXPIRE_MINUTES"] = SupportSecurity.ACCESS_TOKEN_EXPIRE_MINUTES.value
+app_config["ALGORITHMS"] = SupportSecurity.ALGORITHMS.value
 
-app_config = settings.app_config
+# app_config = settings.app_config
 DB_ENGINE = app_config["DB_ENGINE"]
 DB_USERNAME = app_config["DB_USERNAME"]
 DB_PASS = app_config["DB_PASS"]
